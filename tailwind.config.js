d2dae1ca9816d8c8e6df4e4e2f93d26a0bf6bd0f
Becky <bckyjns@gmail.com>
2022-05-09 22:49:44 +0100

    💄  create landing page (#4)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
+    fontFamily: {
+      mono: [
+        'Azeret Mono',
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace',
+      ],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
